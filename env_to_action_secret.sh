9d5735f62fcc8541a2e374e7dde6928b691bee66
thanhtrnnn <thanhbx6a@gmail.com>
2025-11-13 16:34:46 +0700

    refactor: update environment variable names for login credentials in sync workflows

--- env_to_action_secret.sh
+++ env_to_action_secret.sh
@@ -1,3 +1,3 @@
 while IFS='=' read -r key value; do
-  gh secret set "$key" -b"$value" -R thanhtrnnn/codepshit-to-notion-drake
+  gh secret set "$key" -b"$value" -R <USERNAME_HERE>/codepshit-to-docs-notion
 done < ./envs/.trung.env
